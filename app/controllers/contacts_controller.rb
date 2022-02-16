6fcdd14bf6ecc5bc7c95385d8f61682ab8852133
ThePep <jesse.laux@kellogg.northwestern.edu>
2022-02-16 19:47:25 +0000

    draft 3

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,7 +6,14 @@ class ContactsController < ApplicationController
 
   def new
     @contact = Contact.new
+    @company = Company.find(params["company_id"])
+    @contact.company_id = @company.id
   end
-  
+
+  def create
+    @contact = Contact.new(params["contact"])
+    @contact.save
+    redirect_to "/companies/#{@contact.company.id}"
+  end
 
 end
